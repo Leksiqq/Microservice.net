2db386d74ee425c5f52789fbba65192497942b41
Leksiq <leksi@leksi.net>
2024-02-03 19:10:06 +0300

    =

--- Common/Kafka/KafkaProducerAdapter.cs
+++ Common/Kafka/KafkaProducerAdapter.cs
@@ -2,17 +2,19 @@
 
 namespace Net.Leksi.MicroService.Common;
 
-public class KafkaProducerAdapter(KafkaConfig config) : IKafkaProducer
+public class KafkaProducerAdapter : IKafkaProducer
 {
-    private IProducer<string, string> _producer = null!;
-    public void Initialize()
+    private readonly KafkaConfig _config;
+    private readonly IProducer<string, string> _producer = null!;
+    public KafkaProducerAdapter(KafkaConfig config)
     {
-        _producer = new ProducerBuilder<string, string>(config.Properties).Build();
+        _config = config;
+        _producer = new ProducerBuilder<string, string>(_config.Properties).Build();
     }
     public async Task<List<DeliveryResult<string, string>>> ProduceAsync(Message<string, string> message, CancellationToken stoppingToken)
     {
         List<DeliveryResult<string, string>> result = [];
-        foreach(string topic in config.Topics)
+        foreach(string topic in _config.Topics)
         {
             result.Add(await _producer.ProduceAsync(topic, message, stoppingToken));
         }
@@ -20,6 +22,7 @@ public class KafkaProducerAdapter(KafkaConfig config) : IKafkaProducer
     }
     public void Dispose()
     {
+        GC.SuppressFinalize(this);
         _producer?.Dispose();
     }
 }
--- Common/Storage/MinioClientAdapter.cs
+++ Common/Storage/MinioClientAdapter.cs
@@ -5,23 +5,27 @@ using System.Text.RegularExpressions;
 
 namespace Net.Leksi.MicroService.Common;
 
-public class MinioClientAdapter(MinioConfig config) : ICloudClient
+public class MinioClientAdapter : ICloudClient
 {
     private const string s_slash = "/";
+#pragma warning disable SYSLIB1045 // Преобразовать в "GeneratedRegexAttribute".
     private static readonly Regex manySlashes = new("/{2,}");
-    private IMinioClient _minio = null!;
-    public void Initialize()
+#pragma warning restore SYSLIB1045 // Преобразовать в "GeneratedRegexAttribute".
+    private readonly IMinioClient _minio = null!;
+    private readonly MinioConfig _config;
+    public MinioClientAdapter(MinioConfig config)
     {
+        _config = config;
         var builder = new MinioClient()
-            .WithEndpoint(config.Endpoint)
-            .WithCredentials(config.AccessKey, config.SecretKey);
+            .WithEndpoint(_config.Endpoint)
+            .WithCredentials(_config.AccessKey, _config.SecretKey);
         _minio = builder.Build();
     }
     public async Task<bool> FileExists(string path, CancellationToken stoppingToken)
     {
         await EnsureBucketExists(stoppingToken);
         var soArgs = new StatObjectArgs()
-            .WithBucket(config.Bucket)
+            .WithBucket(_config.Bucket)
             .WithObject(GetAbsolutePath(path));
         try
         {
@@ -35,31 +39,32 @@ public class MinioClientAdapter(MinioConfig config) : ICloudClient
     {
         await EnsureBucketExists(stoppingToken);
         var putObjectArgs = new PutObjectArgs()
-            .WithBucket(config.Bucket)
+            .WithBucket(_config.Bucket)
             .WithObject(GetAbsolutePath(path))
             .WithObjectSize(size)
             .WithStreamData(stream)
             .WithContentType(mimeType);
         await _minio.PutObjectAsync(putObjectArgs, stoppingToken);
     }
-    public void Dispose()
-    {
-        _minio?.Dispose();
+    public void Dispose() 
+    { 
+        GC.SuppressFinalize(this);
+        _minio?.Dispose(); 
     }
     private string GetAbsolutePath(string path)
     {
-        return manySlashes.Replace($"{s_slash}{config.Folder ?? string.Empty}{s_slash}{path}", s_slash);
+        return manySlashes.Replace($"{s_slash}{_config.Folder ?? string.Empty}{s_slash}{path}", s_slash);
     }
     private async Task EnsureBucketExists(CancellationToken stoppingToken)
     {
         var beArgs = new BucketExistsArgs()
-            .WithBucket(config.Bucket);
+            .WithBucket(_config.Bucket);
 
         bool found = await _minio.BucketExistsAsync(beArgs, stoppingToken);
         if (!found)
         {
             var mbArgs = new MakeBucketArgs()
-                .WithBucket(config.Bucket);
+                .WithBucket(_config.Bucket);
             await _minio.MakeBucketAsync(mbArgs, stoppingToken);
         }
     }
